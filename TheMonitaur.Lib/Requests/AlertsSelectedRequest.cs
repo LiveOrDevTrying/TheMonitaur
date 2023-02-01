0dc72c2261763b768a48eef3079cdc1fd485c4ec
LiveOrDevTrying <liveordevtrying@gmail.com>
2023-02-01 10:07:21 -0800

    Changed model name

--- TheMonitaur.Lib/Requests/AlertsSelectedRequest.cs
+++ TheMonitaur.Lib/Requests/AlertsSelectedRequest.cs
@@ -1,12 +1,12 @@
 ﻿namespace TheMonitaur.Lib.Requests
 {
     /// <summary>
-    /// An Alerts dismiss request
+    /// An Alerts selected request
     /// </summary>
-    public class AlertsDismissRequest
+    public class AlertsSelectedRequest
     {
         /// <summary>
-        /// The Ids of the Alerts to dismiss
+        /// The Ids of the selected Alerts
         /// </summary>
         public long[] Ids { get; set; }
     }
